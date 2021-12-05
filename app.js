215dee84438d8c94ab4674911aea94312e0bbc7b
Connor Boulton <cjboulton22@gmail.com>
2021-12-05 10:53:11 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,47 @@
+let weather = {
+    apiKey: "28d979dab7bee75e908648eab906633f",
+
+    fetchWeather: function (city) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${this.apiKey}`)
+        .then(res => res.json())
+        .then(data => this.weatherStats(data))
+        .catch(err => {
+            alert(err)
+        })
+    },
+    
+    weatherStats: function (data) {
+        const { name } = data;
+        const { description, icon } = data.weather[0];
+        const { temp, humidity, temp_min, temp_max } = data.main;
+        const { speed } = data.wind;
+        const { country } = data.sys
+
+        document.querySelector(".city").innerHTML = `${name}, ${country}`;
+        document.querySelector(".temp").innerHTML = `${Math.round(temp)} °F`;
+        document.querySelector(".icon-img").src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+        document.querySelector(".description").innerHTML = description
+        document.querySelector(".max-temp").innerHTML = `Max: ${Math.round(temp_max)} °F`
+        document.querySelector(".min-temp").innerHTML = `Min: ${Math.round(temp_min)} °F`
+        document.querySelector(".speed").innerHTML = `Wind Speed: ${Math.round(speed)} mph`
+        document.querySelector(".humidity").innerHTML = `Humidity: ${humidity}%`
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".city-input").value)
+    }
+}
+
+const searchInput = document.querySelector(".city-input")
+const imgBtn = document.querySelector(".img-button")
+
+imgBtn.addEventListener("click", () => {
+    weather.search()
+    searchInput.value = ""
+})
+
+searchInput.addEventListener("keyup", function (event) {
+    if(event.key == "Enter"){
+    weather.search()
+    searchInput.value = ""
+    }
+})
